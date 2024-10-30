31c396e6c8f0bd3a82569a09cd9003d8ff3581c2
Rofida Saad <rofidasaad943@gmail.com>
2024-10-30 23:52:10 +0300

    [29] [Time Beats: 100.00%] [Memory Beats: 33.10%] - LeetPush

---
+++ 29-Divide-Two-Integers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+        int sign = (dividend > 0) == (divisor > 0) ? 1 : -1;
+        long long dvd = labs(dividend), dvs = labs(divisor), res = 0;
+        while (dvd >= dvs) {
+            long long temp = dvs, multiple = 1;
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            dvd -= temp;
+            res += multiple;
+        }
+        return sign * res;
+    }
+};

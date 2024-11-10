8e08f3c1d819f169dc4facdb95715085d1180f92
Rofida Saad <rofidasaad943@gmail.com>
2024-11-11 00:54:33 +0200

    [56] [Time Beats: 70.67%] [Memory Beats: 42.27%] - LeetPush

---
+++ 56-Merge-Intervals.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end());
+        for (auto& i : intervals) {
+            if (res.empty() || res.back()[1] < i[0]) res.push_back(i);
+            else res.back()[1] = max(res.back()[1], i[1]);
+        }
+        return res;
+    }
+};

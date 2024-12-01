6a2f074ab9f41e4a1522e16db4d62f7f42735a44
Rofida Saad <rofidasaad943@gmail.com>
2024-12-02 00:37:55 +0200

    Update 15-3Sum.cpp

--- 15-3Sum.cpp
+++ 15-3Sum.cpp
@@ -1,30 +1,53 @@
-class Solution {
+
+    class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
+     
+       sort(nums.begin(),nums.end());
+
+       vector<vector<int> >v1;
+
+       for(int i =0; i< nums.size(); i++)
+       {
+           if(i>0&& nums[i]==nums[i-1])
+           continue;
+
+             int num1=nums[i];
+
+             int st=i+1;
+             int end=nums.size()-1;             
+             while(st<end)
+             {     
+                 int sum =num1+nums[st]+nums[end];
+                if(sum==0)
+                 {
+                    v1.push_back({nums[i],nums[st],nums[end]});
+                     
+                    int Vstart = nums[st];
+                    int Vend = nums[end];
+
+
+                    while(st<end && nums[st]==Vstart)
+                    {
+                        st++;
+                    } 
+                    while(st<end && nums[end]==Vend)
+                    {
+                        end--;
+                    } 
+                 }
+                 else if (sum < 0)
+                 {
+                     st++;
+                 }
+                 else
+                 {
+                     end--;
+                 }
+             }
+       }
+
+     return v1;
         
-        for (int i=0;i<n-2;i++) {
-            if (i>0&& nums[i] ==nums[i-1]) continue;
-            int left=i+1,right =n- 1;
-            
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if (sum == 0) {
-                    res.push_back({nums[i], nums[left], nums[right]});
-                    while (left<right &&nums[left] == nums[left + 1]) left++;
-                    while (left<right&& nums[right] == nums[right - 1]) right--;
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
-            }
-        }
-        return res;
     }
 };

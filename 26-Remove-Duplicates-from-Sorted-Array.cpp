246dad047c96a841bd5d33eb19b39c6271841dc1
Rofida Saad <rofidasaad943@gmail.com>
2024-12-06 21:11:30 +0200

    Update 26-Remove-Duplicates-from-Sorted-Array.cpp

--- 26-Remove-Duplicates-from-Sorted-Array.cpp
+++ 26-Remove-Duplicates-from-Sorted-Array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int removeDuplicates(std::vector<int>& nums) {
-        if (nums.empty()) {
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()){
             return 0;
         }
         
         int writePointer = 1;
         
-        for (int readPointer = 1; readPointer < nums.size(); readPointer++) {
-            if (nums[readPointer] != nums[readPointer - 1]) {
+        for (int readPointer=1;readPointer < nums.size(); readPointer++) {
+            if(nums[readPointer] !=nums[readPointer - 1]) {
                 nums[writePointer] = nums[readPointer];
-                writePointer++;
+     writePointer++;
             }
         }
         

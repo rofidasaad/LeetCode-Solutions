26776fbafa79f92af7151cedff231d40726bda77
Rofida Saad <rofidasaad943@gmail.com>
2024-12-07 15:44:20 +0200

    [14] [Time Beats: 10.49%] [Memory Beats: 10.91%] - LeetPush

--- 14-Longest-Common-Prefix.cpp
+++ 14-Longest-Common-Prefix.cpp
@@ -2,13 +2,21 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.empty()) return \\;
+
         string prefix = strs[0];
+        int prefixLen = prefix.length();
+
         for (int i = 1; i < strs.size(); i++) {
-            while (strs[i].find(prefix) != 0) {
-                prefix = prefix.substr(0, prefix.size() - 1);
-                if (prefix.empty()) return \\;
+            string s = strs[i];
+            while (prefixLen > s.length() || prefix != s.substr(0, prefixLen)) {
+                prefixLen--;
+                if (prefixLen == 0) {
+                    return \\;
+                }
+                prefix = prefix.substr(0, prefixLen);
             }
         }
-        return prefix;
+
+        return prefix;        
     }
 };

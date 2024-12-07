3397e2f64a78473a0df32c64c3a97a3071426044
Rofida Saad <rofidasaad943@gmail.com>
2024-12-07 14:21:10 +0200

    [37] [Time Beats: 7.05%] [Memory Beats: 19.64%] - LeetPush

--- 37-Sudoku-Solver.cpp
+++ 37-Sudoku-Solver.cpp
@@ -1,20 +1,18 @@
 
 class Solution {
 public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
-    }
-
-private:
-    bool solve(vector<vector<char>>& board) {
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
+    bool solve(vector<vector<char>>& board) {    
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
                 if (board[i][j] == '.') {
-                    for (char c = '1'; c <= '9'; ++c) {
+                    for (char c = '1'; c <= '9'; c++) {
                         if (isValid(board, i, j, c)) {
                             board[i][j] = c;
-                            if (solve(board)) return true;
-                            board[i][j] = '.';
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
                         }
                     }
                     return false;
@@ -25,10 +23,18 @@ private:
     }
 
     bool isValid(vector<vector<char>>& board, int row, int col, char c) {
-        for (int i = 0; i < 9; ++i) {
-            if (board[i][col] == c || board[row][i] == c || board[row/3*3 + i/3][col/3*3 + i%3] == c)
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == c) 
+                return false;
+            if (board[row][i] == c) 
+                return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) 
                 return false;
         }
         return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
     }
 };

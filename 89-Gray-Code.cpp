618a33a77c9450155fb73989130d5a11eff128f0
Rofida Saad <rofidasaad943@gmail.com>
2024-12-25 22:35:24 +0200

    Update 89-Gray-Code.cpp

--- 89-Gray-Code.cpp
+++ 89-Gray-Code.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    vector<int> vect;
-    void helper(int &a,int n)
-    {
-        a^=1<<n;
-        vect.push_back(a);
-        for(int c=0;c<n;c++)
-            helper(a,c);
+    void buildGrayCode(int bitCount,vector<int>& sequence) {
+        if (bitCount==1) {
+        sequence.push_back(0);
+           sequence.push_back(1);
+            return;
+        }
+        buildGrayCode(bitCount-1,sequence);
+
+        int lastIndex=sequence.size() - 1;
+
+        while (lastIndex >= 0) {
+            int grayValue=(1<<(bitCount-1))| sequence[lastIndex];
+            sequence.push_back(grayValue);
+            lastIndex--;
+        }
+        return;
     }
-    vector<int> grayCode(int n) 
-    {
-        int a=0;
-        vect.push_back(a);
-        for(int c=0;c<n;c++)
-            helper(a,c);
-        return vect;
+    vector<int> grayCode(int numBits) {
+        vector<int> graySequence;
+        buildGrayCode(numBits, graySequence);
+        return graySequence;   
     }
 };

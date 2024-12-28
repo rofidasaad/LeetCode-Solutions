440fdafeddda2362d185916d1d3aee8728ee202c
Rofida Saad <rofidasaad943@gmail.com>
2024-12-28 03:57:07 +0200

    Update 76-Minimum-Window-Substring.cpp

--- 76-Minimum-Window-Substring.cpp
+++ 76-Minimum-Window-Substring.cpp
@@ -1,16 +1,1 @@
-class Solution {
-public:
-    string minWindow(string s, string t) {
-    unordered_map<char, int> mp;
-    for (char c : t) mp[c]++;
-    int required = t.size(), l = 0, r = 0, minLen = INT_MAX, start = 0;
-    while (r < s.size()) {
-        if (mp[s[r++]]-- > 0) required--;
-        while (required == 0) {
-            if (r - l < minLen) minLen = r - l, start = l;
-            if (++mp[s[l++]] > 0) required++;
-        }
-    }
-    return minLen == INT_MAX ? \\ : s.substr(start, minLen);
-}
-};
+

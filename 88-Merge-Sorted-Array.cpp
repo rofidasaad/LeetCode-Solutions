a43177c962e69cf07ab7b271eb3b0b7f31025117
Rofida Saad <rofidasaad943@gmail.com>
2024-12-28 03:41:01 +0200

    Update 88-Merge-Sorted-Array.cpp

--- 88-Merge-Sorted-Array.cpp
+++ 88-Merge-Sorted-Array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1, j = n - 1, k = m + n - 1;
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+    void merge(vector<int>& num1, int m, vector<int>& num2, int n) {
+        int i=m-1, j=n-1, k =m+n-1;
+        while(i>= 0 && j >= 0) {
+            if(num1[i] > num2[j]) {
+      num1[k--] = num1[i--];
             } else {
-                nums1[k--] = nums2[j--];
+                num1[k--] = num2[j--];
             }
         }
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+        while (j>=0) {
+            num1[k--] = num2[j--];
         }
     }
 };
